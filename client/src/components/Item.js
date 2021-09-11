7542054eb1a4fa474852d699f3432249e6b34caa
Elise Brown <elisemarie88@gmail.com>
2021-09-11 12:27:54 -0600

    Items, Item and delete

--- client/src/components/Item.js
+++ client/src/components/Item.js
@@ -1,11 +1,18 @@
 import React from "react";
+import axios from "axios";
+import { useHistory } from "react-router";
+import { Button } from "semantic-ui-react";
 
-const Item = () => {
+const Item = ({item , deleteItem}) => {
+
   return (
     <div>
       <h1>
-        Item
+        {item.name}
       </h1>
+      <p>{item.description}</p>
+      <p>{`$${item.price}`}</p>
+      <Button onClick={()=> deleteItem(item.id)}>Delete</Button>
     </div>
   );
 };
