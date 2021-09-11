45b89f2913d110470b2fd1fff76bca23bdb7577d
Elise Brown <elisemarie88@gmail.com>
2021-09-11 10:01:09 -0600

    Browser Router Navigation

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { Fragment, } from 'react';
+import { Route, Switch, } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <>
+      <NavBar />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route path="/groups" component={Groups} />
+          <Route path="/groups/new" component={NewGroup} />
+          <Route path="/groups/:id" component={Group} />
+          <Route path="/groups/:id/edit" component={EditGroup} />
+          <Route path="/groups/:group_id/items" component={Items} />
+          <Route path="/groups/:group_id/items/new" component={NewItem} />
+          <Route path="/groups/:group_id/items/:id" component={Item} />
+          <Route path="/groups/:group_id/items/:id/edit" component={EditItem} />
+          <Route path="/services" component={Services} />
+          <Route path="/services/new" component={NewService} />
+          <Route path="/services/:id" component={Service} />
+          <Route path="/services/:id/edit" component={EditService} />
+        </Switch>
+      </>
   );
 }
 
