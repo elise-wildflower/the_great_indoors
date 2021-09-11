afb1bad3d0797dd141785ee08e012d46e53c66fe
Michaela Sivertsen <mjsivertsen@msn.com>
2021-09-11 12:24:04 -0600

    groups and group show and delete is good

--- client/src/components/Group.js
+++ client/src/components/Group.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { Button } from "semantic-ui-react";
 
-const Group = () => {
+const Group = ( {group, deleteGroup} ) => {
   return (
     <div>
-      <h1>
-        Group say what?
-      </h1>
+      <h1>{group.name}</h1>
+      <p>{group.description}</p>
+      <Link to={`/groups/${group.id}/items`}>{`Go to ${group.name}`}</Link>
+      < Button basic color='red' onClick={() => deleteGroup(group.id)}>Delete</Button>
     </div>
   );
 };
